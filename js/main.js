f8a80436502c42ee33c48cc3d7806320af9a50a1
KoGor <kg@source.fake>
2015-07-02 16:10:00 +0300

    Hack forcing tiles redraw added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var map = L.map('map').setView([55.743, 37.62], 11);
-    L.tileLayer.grayscale('http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png', {
+var tileLayer = L.tileLayer.grayscale('http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png', {
             attribution: ' © <a href="http://sputnik.ru">Спутник</a> | © <a href="http://www.openstreetmap.org/copyright">Openstreetmap</a>',
             maxZoom: 18,
             minZoom: 11,
             maxBounds: [[55.123, 36.800], [56.200, 38.430]]
-        }).addTo(map);
+        });
+tileLayer.addTo(map);
 
 var streetsLayer = L.geoJson(streetsData.features, {
     style: function (feature) {
@@ -13,10 +14,7 @@ var streetsLayer = L.geoJson(streetsData.features, {
         return style[feature.properties.stylegroup];
     },
     onEachFeature: function (feature, layer) {
-        //console.log(feature);
         feature.layer = layer;
-        //console.log(layer);
-        //console.log(feature);
            layer.bindPopup('Название: ' + '<a href="' + feature.properties.st_wiki + '" target="_blank" class="map-popup-link">' + feature.properties.name + '</a> </br>'
                   + 'Память: ' + '<a href="' + feature.properties.hero_wiki + '" target="_blank" class="map-popup-link">' + feature.properties.memory + '</a>');
     }
@@ -29,26 +27,32 @@ map.on('zoomend', function() {
         return style[feature.properties.stylegroup];
     });
 });
-    // Add fuse search control
-    var options = {
-        position: 'topright',
-        title: 'Chercher',
-        placeholder: 'Улицы и Герои',
-        maxResultLength: 7,
-        threshold: 0.2,
-        showInvisibleFeatures: false,
-        showResultFct: function(feature, container) {
-            var props = feature.properties;
-            var name = L.DomUtil.create('b', null, container);
-            name.innerHTML = props.name;
 
-            container.appendChild(L.DomUtil.create('br', null, container));
+// Hack for forcing tiles redraw
+tileLayer.on('load', function() {
+    map.panBy([1, 0]);
+});
 
-            container.appendChild(document.createTextNode(props.memory));
-        }
-    };
-    var fuseSearchCtrl = L.control.fuseSearch(options);
-    map.addControl(fuseSearchCtrl);
+// Add fuse search control
+var options = {
+    position: 'topright',
+    title: 'Chercher',
+    placeholder: 'Улицы и Герои',
+    maxResultLength: 7,
+    threshold: 0.2,
+    showInvisibleFeatures: false,
+    showResultFct: function(feature, container) {
+        var props = feature.properties,
+            name = L.DomUtil.create('b', null, container);
+        name.innerHTML = props.name;
+
+        container.appendChild(L.DomUtil.create('br', null, container));
+        container.appendChild(document.createTextNode(props.memory));
+    }
+};
+
+var fuseSearchCtrl = L.control.fuseSearch(options);
 fuseSearchCtrl.indexFeatures(streetsData.features, ['name', 'memory']);
+map.addControl(fuseSearchCtrl);
 
 streetsLayer.addTo(map);
